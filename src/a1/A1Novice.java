88607bf93e422d8939855f86cc6cb94350ef4556
canain <canain@canain-pc.wireless-1x.unc.edu>
2019-09-03 01:29:52 -0400

    testing first part of novice

--- src/a1/A1Novice.java
+++ src/a1/A1Novice.java
@@ -9,6 +9,25 @@ public class A1Novice {
 		Scanner scan = new Scanner(System.in);
 
 		// Your code follows here.
+		int a = scan.nextInt();
+		
+		String b = scan.next();
+		
+		String c = scan.next();
+		
+		int d = scan.nextInt();
+		
+		int e = scan.nextInt();
+		
+		String f = scan.next();
+		
+		double g = scan.nextDouble();
+		
+		System.out.println(b.charAt(0) + ". " + c + ": ");
+		
+//		let total = (d, e, g): double => {
+
+		
 		
 	}
 }
